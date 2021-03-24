aa8fccc77626afae8094ae3e535beece02db9012
Mustafa Kahraman <kahramanmstafa@gmail.com>
2021-03-24 13:55:04 +0300

    Sign in process working

--- src/actions/signedInUser.js
+++ src/actions/signedInUser.js
@@ -1,7 +1,7 @@
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 
-function signInAction({userName}){
+function signInAction(userName){
     return {
         type: SIGN_IN,
         userId: userName
@@ -14,10 +14,10 @@ function signOutAction(){
     }
 }
 
-export function handleSignIn({userName}){
+export function handleSignIn(userName){
     return (dispatch) => {
         //* Do side effects if exists
-        return dispatch(signInAction({userName}));
+        return dispatch(signInAction(userName));
     }
 }
 
--- src/components/UserSelectionList.js
+++ src/components/UserSelectionList.js
@@ -1,18 +1,54 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import { handleFetchUsers, handleAddUser } from "../actions/users"
+import {handleSignIn} from "../actions/signedInUser"
 import { DropdownList } from 'react-widgets';
 
 class UserSelectionList extends Component {
+  componentDidMount(){
+    this.props.dispatch(handleFetchUsers());
+    const userIds = Object.keys(this.props.users);
+
+    this.setState(() => ({
+      selectedUserId: userIds[0]
+    }))
+  }
+
+  state = {
+    selectedUserId: ""
+  }
+
+  changeUser = (e) => {
+    this.setState(() => ({
+      selectedUserId: e.target.value
+    }))
+  }
+
+  signIn = (e) => {
+    e.preventDefault();
+    this.props.dispatch(handleSignIn(this.state.selectedUserId));
+    alert("Signed In");
+  }
+
   render(){
-    let colors = ['orange', 'red', 'blue', 'purple']
+    const {users} = this.props;
 
     return(
-      <select class="selectpicker">
-        <option>Mustard</option>
-        <option>Ketchup</option>
-        <option>Barbecue</option>
-      </select>
+      <form onSubmit={this.signIn}>
+        <label htmlFor="users">Choose a user:</label>
+        <select id="users" value={this.state.selectedUserId} onChange={this.changeUser}>
+          {Object.keys(users).map((userId) =>
+            <option value={userId} key={userId} >{users[userId].name}</option>
+          )}
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
     )
   }
 }
 
-export default UserSelectionList;
+function mapStateToProps({users}){
+  return {users};
+}
+
+export default connect(mapStateToProps)(UserSelectionList);
